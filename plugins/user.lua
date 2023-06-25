3c05957ff8ad0dc5c46bf67a24af3adeedaccdf2
JalukOne <jalukone@outlook.com>
2023-06-25 04:25:05 -0500

    Add TokyoNight Storm

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,10 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    opts = {},
+  },
 }
